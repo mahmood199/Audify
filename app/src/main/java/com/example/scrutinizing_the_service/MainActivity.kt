65312da1d1c5493a096555f4d03b45b83e826a18
MSI KATANA <mohammed.mahmood@semaai.com>
2023-02-25 16:01:54 +0530

    loggin

--- app/src/main/java/com/example/scrutinizing_the_service/MainActivity.kt
+++ app/src/main/java/com/example/scrutinizing_the_service/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.scrutinizing_the_service
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.scrutinizing_the_service.databinding.ActivityMainBinding
 import com.example.scrutinizing_the_service.services.RussianService
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
@@ -27,6 +32,7 @@ class MainActivity : AppCompatActivity() {
     private fun setClickListeners() {
         with(binding) {
             btnStartService.setOnClickListener {
+                Log.d(TAG, Thread.currentThread().name)
                 startService(russianServiceIntent)
             }
             btnStopService.setOnClickListener {
