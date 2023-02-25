c9b3b76a68dd65b5e62c1729ef3dfdebbc5b7339
Mohammed Mahmood Ahmed <gaudentas199@gmail.com>
2023-02-25 16:03:02 +0530

    Feature/lifecycle services (#2)
    * Added lifecycle services dependency.
    
    * Created smart services which extends lifecycle services
    
    * loggin
    
    * loggin
    
    * loggin

--- app/src/main/java/com/example/scrutinizing_the_service/services/RussianService.kt
+++ app/src/main/java/com/example/scrutinizing_the_service/services/RussianService.kt
@@ -3,13 +3,17 @@ package com.example.scrutinizing_the_service.services
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
-import android.os.IInterface
-import android.os.Parcel
-import java.io.FileDescriptor
+import android.util.Log
 
 class RussianService : Service() {
 
+    companion object {
+        const val TAG = "RussianService"
+    }
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d(TAG, "Started")
+        Log.d(TAG, Thread.currentThread().name)
         return super.onStartCommand(intent, flags, startId)
     }
 
@@ -18,6 +22,7 @@ class RussianService : Service() {
     }
 
     override fun onDestroy() {
+        Log.d(TAG, "Stopped")
         super.onDestroy()
     }
 
