d15deeb759a95d1a74bdd46ed95f586f6dba2d45
MSI KATANA <mohammed.mahmood@semaai.com>
2023-02-25 14:23:31 +0530

    enabled viewBinding for project

--- app/src/main/java/com/example/scrutinizing_the_service/MainActivity.kt
+++ app/src/main/java/com/example/scrutinizing_the_service/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.scrutinizing_the_service
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.scrutinizing_the_service.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
+
     }
 }
