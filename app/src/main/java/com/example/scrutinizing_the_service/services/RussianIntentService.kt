01b6fefb0961596e5f9e38a2798f0779f2d39e28
mahmood <gaudentas199@gmail.com>
2023-02-25 13:01:01 -0800

    intent service demo
    Signed-off-by: mahmood <gaudentas199@gmail.com>

---
+++ app/src/main/java/com/example/scrutinizing_the_service/services/RussianIntentService.kt
@@ -0,0 +1,34 @@
+package com.example.scrutinizing_the_service.services
+
+import android.app.IntentService
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+
+class RussianIntentService : IntentService("RussianIntentService") {
+
+    companion object {
+        const val TAG = "LEARNING IntentService"
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d(TAG, "Started")
+        Log.d(TAG, Thread.currentThread().name)
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onHandleIntent(p0: Intent?) {
+
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "Stopped")
+        super.onDestroy()
+    }
+
+}
