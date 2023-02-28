dacc6b59baafba039c5c64aa7e681a4bfc59824d
MSI KATANA <mohammed.mahmood@semaai.com>
2023-02-28 15:40:32 +0530

    notif builder classes upgraded
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>

--- app/src/main/java/com/example/scrutinizing_the_service/notifs/OreoNotificationBuilder.kt
+++ app/src/main/java/com/example/scrutinizing_the_service/notifs/OreoNotificationBuilder.kt
@@ -1,12 +1,59 @@
 package com.example.scrutinizing_the_service.notifs
 
-import android.app.Service
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.scrutinizing_the_service.R
 
-class OreoNotificationBuilder : NotificationBuilder {
+@RequiresApi(Build.VERSION_CODES.O)
+class OreoNotificationBuilder(context: Context) : NotificationBuilder {
 
+    companion object {
+        const val CHANNEL_1_ID = "CHANNEL_1_ID"
+        const val CHANNEL_2_ID = "CHANNEL_2_ID"
+    }
 
-    override fun createNotification(context: Service) {
+    private val notificationManager by lazy {
+        NotificationManagerCompat.from(context)
+    }
 
+    override fun createNotification(context: Context) {
+        val notification = Notification.Builder(context, CHANNEL_1_ID)
+            .setSmallIcon(R.drawable.placeholder)
+            .setContentTitle("My First Notification Title")
+            .setContentText("First Notification Description")
+            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+            .build()
+        notificationManager.notify(1, notification)
+    }
+
+    override fun createChannel(context: Context) {
+        val channel1 = NotificationChannel(
+            CHANNEL_1_ID,
+            "some_identifier_1",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        channel1.description = "This is First Notification Channel description"
+
+        val channel2 = NotificationChannel(
+            CHANNEL_2_ID,
+            "some_identifier_2",
+            NotificationManager.IMPORTANCE_LOW
+        )
+        channel2.description = "This is Second Notification Channel description"
+
+        val manager = context.getSystemService(NotificationManager::class.java)
+        manager.createNotificationChannel(channel1)
+        manager.createNotificationChannel(channel2)
+
+/*
+        manager.createNotificationChannels(mutableListOf(channel1, channel2))
+*/
     }
 
 
