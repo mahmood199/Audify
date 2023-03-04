0b2d4d6a13ae4264a8a2d468fc2e548c7937707a
Mohammed Mahmood Ahmed <gaudentas199@gmail.com>
2023-03-04 18:58:41 +0530

    Music Locator 1 (#13)
    * testing 1
    
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>
    
    * Failed. Not all music fetched
    
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>
    
    * Getting the list of device internal ringtones
    
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>
    
    * Getting the list of device internal ringtones
    
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>
    
    * showing paths
    
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>
    
    * showing paths
    
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>
    
    * permissions
    
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>
    
    * Yesss got the files.
    
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>
    
    ---------
    
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>
    Co-authored-by: MSI KATANA <mohammed.mahmood@semaai.com>

--- app/src/main/java/com/example/scrutinizing_the_service/ui/music/MusicPlayerActivity.kt
+++ app/src/main/java/com/example/scrutinizing_the_service/ui/music/MusicPlayerActivity.kt
@@ -1,9 +1,17 @@
 package com.example.scrutinizing_the_service.ui.music
 
+import android.Manifest.permission.*
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.example.scrutinizing_the_service.data.Song
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.scrutinizing_the_service.databinding.ActivityMusicPlayerBinding
+import com.example.scrutinizing_the_service.platform.MusicLocatorV2
+import com.example.scrutinizing_the_service.platform.MusicLocator
+import com.example.scrutinizing_the_service.platform.MusicLocatorV3
 
 class MusicPlayerActivity : AppCompatActivity() {
 
@@ -15,36 +23,77 @@ class MusicPlayerActivity : AppCompatActivity() {
         SongsAdapter()
     }
 
+    companion object {
+        const val CODE = 1
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Log.i("Permission: ", "Granted")
+            } else {
+                Log.i("Permission: ", "Denied")
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         setAdapter()
-        Thread.sleep(1000)
-        (binding.rvMusicItems.adapter as SongsAdapter).addNewItems(
-            listOf(
-                Song("Song 1", " Artist 1", false),
-                Song("Song 2", " Artist 2", false),
-                Song("Song 3", " Artist 3", false),
-                Song("Song 4", " Artist 4", false),
-                Song("Song 5", " Artist 5", false),
-                Song("Song 6", " Artist 6", false),
-            )
-        )
 
         binding.btnAction.setOnClickListener {
-            (binding.rvMusicItems.adapter as SongsAdapter).addNewItems(
-                listOf(
-                    Song("Song 1", " Artist 1", false),
-                    Song("Song 2", " Artist 2", false),
-                    Song("Song 3", " Artist 3", false),
-                    Song("Song 4", " Artist 4", false),
-                    Song("Song 5", " Artist 5", false),
-                    Song("Song 6", " Artist 6", false),
-                )
-            )
+            checkForPermission()
         }
 
 
+    }
+
+    private fun checkForPermission() {
+        if (
+            ContextCompat.checkSelfPermission(
+                this,
+                READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            (binding.rvMusicItems.adapter as SongsAdapter).addNewItems(
+                MusicLocatorV2.getAllAudio(this)
+            )
+        } else {
+            requestPermission()
+        }
+    }
+
+    private fun requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, READ_EXTERNAL_STORAGE)
+        ) {
+
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                listOf(
+                    READ_EXTERNAL_STORAGE,
+                    WRITE_EXTERNAL_STORAGE,
+                    CAMERA,
+                ).toTypedArray(), CODE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CODE) {
+            (binding.rvMusicItems.adapter as SongsAdapter).addNewItems(
+                MusicLocatorV2.getAllAudio(this)
+            )
+        } else {
+
+        }
     }
 
     private fun setAdapter() {
