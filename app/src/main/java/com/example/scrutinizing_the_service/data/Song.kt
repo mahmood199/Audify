590a19788afdbe3258f56fc26f32467a9db0f12f
MSI KATANA <mohammed.mahmood@semaai.com>
2023-03-08 00:29:43 +0530

    For sending data across intents
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>

--- app/src/main/java/com/example/scrutinizing_the_service/data/Song.kt
+++ app/src/main/java/com/example/scrutinizing_the_service/data/Song.kt
@@ -1,9 +1,13 @@
 package com.example.scrutinizing_the_service.data
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Song(
     val name: String,
     val artist: String,
-    val isFavourite: Boolean,
+    val isFavourite: Boolean = false,
     val path: String = "",
     val duration: Int = 0
-)
+) : Parcelable
