9a5e64be0bd5857b9f171d233892944737adfb19
Mohammed Mahmood Ahmed <gaudentas199@gmail.com>
2023-03-08 12:33:57 +0530

    Previously it was crashing and not giving the entire song list but now it is working (#21)

--- app/src/main/java/com/example/scrutinizing_the_service/platform/MusicLocatorV2.kt
+++ app/src/main/java/com/example/scrutinizing_the_service/platform/MusicLocatorV2.kt
@@ -2,65 +2,66 @@ package com.example.scrutinizing_the_service.platform
 
 import android.content.Context
 import android.database.MergeCursor
+import android.media.MediaDataSource
 import android.media.MediaMetadataRetriever
 import android.net.Uri
 import android.provider.MediaStore
 import android.util.Log
 import com.example.scrutinizing_the_service.data.Song
+import java.io.File
 import java.util.*
 
 
-//Some what working. This is lsiting all the device ringtones
+//Some what working. This is listing all the device ringtones
 object MusicLocatorV2 {
 
     private const val TAG = "MusicLocatorV2"
 
     fun getAllAudio(context: Context): List<Song> {
         val files: MutableList<Song> = ArrayList()
-        try {
-            val columns = arrayOf(
-                MediaStore.Audio.Media.DATA,
-                MediaStore.Audio.Media.DISPLAY_NAME,
-                MediaStore.Audio.Media.ALBUM,
-                MediaStore.Audio.Media.ARTIST
+        val columns = arrayOf(
+            MediaStore.Audio.Media.DATA,
+            MediaStore.Audio.Media.DISPLAY_NAME,
+            MediaStore.Audio.Media.ALBUM,
+            MediaStore.Audio.Media.ARTIST
+        )
+        val cursor = MergeCursor(
+            arrayOf(
+                context.contentResolver.query(
+                    MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                    columns,
+                    null,
+                    null,
+                    null
+                ),
             )
-            val cursor = MergeCursor(
-                arrayOf(
-                    context.contentResolver.query(
-                        MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                        columns,
-                        null,
-                        null,
-                        null
-                    ),
-                )
-            )
-            cursor.moveToFirst()
-            files.clear()
-            while (!cursor.isAfterLast) {
-                var path = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA))
-                val name = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME))
-                var album = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM))
-                val artist = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST))
-                val lastPoint = path.lastIndexOf(".")
-                path = path.substring(0, lastPoint) + path.substring(lastPoint).lowercase(Locale.getDefault())
-                Log.d(TAG, path)
-                val duration = getTotalDurationOfAudio(context, path)
-                files.add(Song(name, artist, false, path, duration))
-                cursor.moveToNext()
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
+        )
+        cursor.moveToFirst()
+        files.clear()
+        while (!cursor.isAfterLast) {
+            var path = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA))
+            val name = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME))
+            var album = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM))
+            val artist = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST))
+            val lastPoint = path.lastIndexOf(".")
+            path = path.substring(0, lastPoint) + path.substring(lastPoint)
+                .lowercase(Locale.getDefault())
+            Log.d(TAG, path)
+            val duration = getTotalDurationOfAudio(context, path)
+            files.add(Song(name, artist, false, path, duration))
+            cursor.moveToNext()
         }
+
         return files
     }
 
-    private val mmr = MediaMetadataRetriever()
+    private val metadataRetriever = MediaMetadataRetriever()
 
     private fun getTotalDurationOfAudio(context: Context, pathStr: String): Int {
-        val uri = Uri.parse(pathStr)
-        mmr.setDataSource(context, uri)
-        val durationStr = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
+        val file = File(pathStr)
+        val uri = Uri.fromFile(file)
+        metadataRetriever.setDataSource(context, uri)
+        val durationStr = metadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
         return durationStr?.toInt()?.div(1000) ?: 1
     }
 
