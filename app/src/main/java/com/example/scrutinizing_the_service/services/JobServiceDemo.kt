e1a971632ccd20b62c2697b2cb8f345efd1c2fcf
MSI KATANA <mohammed.mahmood@semaai.com>
2023-03-16 22:00:11 +0530

    job service implementation
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>

---
+++ app/src/main/java/com/example/scrutinizing_the_service/services/JobServiceDemo.kt
@@ -0,0 +1,16 @@
+package com.example.scrutinizing_the_service.services
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+
+class JobServiceDemo: JobService() {
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+}
