48fa7c35b18017147dd4bae6fb8b5011aa277148
Mohammed Mahmood Ahmed <gaudentas199@gmail.com>
2023-03-19 02:38:53 +0530

    Revert "Media player using Exo-player (#23)"
    This reverts commit 73f5e878ba7cbdb2b47c2e66a96de1c329b12273.

--- app/src/main/java/com/example/scrutinizing_the_service/ui/exo_music/SongClickListener.kt
+++
@@ -1,7 +0,0 @@
-package com.example.scrutinizing_the_service.ui.exo_music
-
-import com.example.scrutinizing_the_service.data.Song
-
-sealed class SongClickListener {
-    data class ItemClicked(val song: Song, val position: Int, val totalItem: Int) : SongClickListener()
-}
