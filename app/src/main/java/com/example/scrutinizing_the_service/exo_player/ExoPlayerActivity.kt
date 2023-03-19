d409d1c7dedb12c98744171aedef80af0fb5cc3a
MSI KATANA <mohammed.mahmood@semaai.com>
2023-03-20 02:01:30 +0530

    Music Player Service setup done
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>

--- app/src/main/java/com/example/scrutinizing_the_service/exo_player/ExoPlayerActivity.kt
+++ app/src/main/java/com/example/scrutinizing_the_service/exo_player/ExoPlayerActivity.kt
@@ -3,6 +3,7 @@ package com.example.scrutinizing_the_service.exo_player
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.scrutinizing_the_service.R
+import com.example.scrutinizing_the_service.exo_player.service.MusicPlayerService
 
 class ExoPlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/scrutinizing_the_service/util/PackageAccessValidator.kt
@@ -0,0 +1,12 @@
+package com.example.scrutinizing_the_service.util
+
+object PackageAccessValidator {
+
+    fun allowBrowsing(
+        clientPackageName: String = "",
+        clientUid: String = ""
+    ): Boolean {
+        return true
+    }
+
+}
