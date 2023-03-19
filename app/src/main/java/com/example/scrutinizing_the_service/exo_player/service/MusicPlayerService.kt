3bcc6076a8429a254c9a1112952f701db40accc0
MSI KATANA <mohammed.mahmood@semaai.com>
2023-03-20 01:49:31 +0530

    Initialising the Media Session in MusicPlayerService.kt
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>

--- app/src/main/java/com/example/scrutinizing_the_service/exo_player/service/MusicPlayerService.kt
+++ app/src/main/java/com/example/scrutinizing_the_service/exo_player/service/MusicPlayerService.kt
@@ -2,9 +2,40 @@ package com.example.scrutinizing_the_service.exo_player.service
 
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.session.MediaSessionCompat
+import android.support.v4.media.session.PlaybackStateCompat
 import androidx.media.MediaBrowserServiceCompat
 
 class MusicPlayerService : MediaBrowserServiceCompat() {
+
+    companion object {
+        const val TAG = "MusicPlayerService"
+    }
+
+    private lateinit var mediaSession : MediaSessionCompat
+    private lateinit var stateBuilder: PlaybackStateCompat.Builder
+
+    override fun onCreate() {
+        super.onCreate()
+        initializeMediaSession()
+    }
+
+    private fun initializeMediaSession() {
+        mediaSession = MediaSessionCompat(baseContext, TAG).apply {
+            setFlags(MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS or
+            MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS)
+
+            stateBuilder = PlaybackStateCompat.Builder().setActions(
+                PlaybackStateCompat.ACTION_PLAY or
+                PlaybackStateCompat.ACTION_PLAY_PAUSE
+            )
+            setPlaybackState(stateBuilder.build())
+
+            setCallback(MusicSessionCallback())
+
+            setSessionToken(sessionToken)
+        }
+    }
 
     override fun onGetRoot(
         clientPackageName: String,
