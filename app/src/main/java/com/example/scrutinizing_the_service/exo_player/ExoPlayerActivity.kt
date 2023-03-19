22107f1d50225f53a2df0258787174eb09628818
MSI KATANA <mohammed.mahmood@semaai.com>
2023-03-20 02:07:42 +0530

    binding setup also done
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>

--- app/src/main/java/com/example/scrutinizing_the_service/exo_player/ExoPlayerActivity.kt
+++ app/src/main/java/com/example/scrutinizing_the_service/exo_player/ExoPlayerActivity.kt
@@ -3,11 +3,17 @@ package com.example.scrutinizing_the_service.exo_player
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.scrutinizing_the_service.R
+import com.example.scrutinizing_the_service.databinding.ActivityExoPlayerBinding
 import com.example.scrutinizing_the_service.exo_player.service.MusicPlayerService
 
 class ExoPlayerActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityExoPlayerBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_exo_player)
+        setContentView(binding.root)
+
     }
 }
