878346e043730e099c88a19b701d882b74f6f7a2
MSI KATANA <mohammed.mahmood@semaai.com>
2023-03-22 01:11:39 +0530

    client setup done
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>

--- app/src/main/java/com/example/scrutinizing_the_service/exo_player/ExoPlayerActivity.kt
+++ app/src/main/java/com/example/scrutinizing_the_service/exo_player/ExoPlayerActivity.kt
@@ -1,13 +1,17 @@
 package com.example.scrutinizing_the_service.exo_player
 
 import android.content.ComponentName
+import android.media.AudioManager
 import android.media.browse.MediaBrowser
 import android.media.session.MediaController
 import android.media.session.PlaybackState
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaControllerCompat
+import android.support.v4.media.session.PlaybackStateCompat
 import androidx.annotation.RequiresApi
 import com.example.scrutinizing_the_service.R
 import com.example.scrutinizing_the_service.databinding.ActivityExoPlayerBinding
@@ -16,14 +20,18 @@ import com.example.scrutinizing_the_service.exo_player.service.MusicPlayerServic
 @RequiresApi(Build.VERSION_CODES.O)
 class ExoPlayerActivity : AppCompatActivity() {
 
+    private lateinit var controllerCallbacks: MediaControllerCompat.Callback
+
     private val binding by lazy {
         ActivityExoPlayerBinding.inflate(layoutInflater)
     }
 
-    private val mediaBrowser by lazy {
-        MediaBrowser(
+    private val mediaBrowser: MediaBrowserCompat by lazy {
+        MediaBrowserCompat(
             this,
             ComponentName(this, MusicPlayerService::class.java),
+            connectionCallbacks,
+            null
         )
     }
 
@@ -33,42 +41,27 @@ class ExoPlayerActivity : AppCompatActivity() {
 
     }
 
-    private val connectionCallbacks = object : MediaBrowser.ConnectionCallback() {
-        override fun onConnected() {
-
-            // Get the token for the MediaSession
-            mediaBrowser.sessionToken.also { token ->
-
-                // Create a MediaControllerCompat
-                val mediaController = MediaController(
-                    this@ExoPlayerActivity,
-                    token
-                )
-
-                // Save the controller
-                MediaControllerCompat.setMediaController(
-                    this@ExoPlayerActivity,
-                    mediaController as MediaControllerCompat
-                )
-            }
-
-            // Finish building the UI
-            buildTransportControls()
-        }
-
-        override fun onConnectionSuspended() {
-            // The Service has crashed. Disable transport controls until it automatically reconnects
-        }
-
-        override fun onConnectionFailed() {
-            // The Service has refused our connection
-        }
+    override fun onStart() {
+        super.onStart()
+        mediaBrowser.connect()
     }
+
+    override fun onResume() {
+        super.onResume()
+        volumeControlStream = AudioManager.STREAM_MUSIC
+    }
+
+    override fun onStop() {
+        super.onStop()
+        MediaControllerCompat.getMediaController(this).unregisterCallback(controllerCallbacks)
+        mediaBrowser.disconnect()
+    }
+
 
     fun buildTransportControls() {
         val mediaController = MediaControllerCompat.getMediaController(this@ExoPlayerActivity)
         // Grab the view for the play/pause button
-        playPause = findViewById<ImageView>(R.id.play_pause).apply {
+        with(binding.btnPlayPause) {
             setOnClickListener {
                 // Since this is a play/pause button, you'll need to test the current state
                 // and choose the action accordingly
@@ -87,6 +80,50 @@ class ExoPlayerActivity : AppCompatActivity() {
         val pbState = mediaController.playbackState
 
         // Register a Callback to stay in sync
-        mediaController.registerCallback(contr)
+        mediaController.registerCallback(controllerCallback)
     }
+
+
+    private val connectionCallbacks = object : MediaBrowserCompat.ConnectionCallback() {
+        override fun onConnected() {
+
+            val mediaController = MediaControllerCompat(
+                this@ExoPlayerActivity,
+                mediaBrowser.sessionToken
+            )
+
+            MediaControllerCompat.setMediaController(
+                this@ExoPlayerActivity,
+                mediaController
+            )
+
+            buildTransportControls()
+        }
+
+        override fun onConnectionSuspended() {
+            // The Service has crashed. Disable transport controls until it automatically reconnects
+        }
+
+        override fun onConnectionFailed() {
+            // The Service has refused our connection
+        }
+    }
+
+    private var controllerCallback = object : MediaControllerCompat.Callback() {
+
+        override fun onMetadataChanged(metadata: MediaMetadataCompat?) {
+            super.onMetadataChanged(metadata)
+        }
+
+        override fun onPlaybackStateChanged(state: PlaybackStateCompat?) {
+            super.onPlaybackStateChanged(state)
+        }
+
+        override fun onSessionDestroyed() {
+            super.onSessionDestroyed()
+            mediaBrowser.disconnect()
+        }
+
+    }
+
 }
