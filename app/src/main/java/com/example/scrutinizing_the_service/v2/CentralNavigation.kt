a7749d77e6de17a761c8d3bd930b6b4cfa4bd234
Mohammed Mahmood Ahmed <gaudentas199@gmail.com>
2023-09-07 22:16:03 +0530

    Added animation for flaoting action button hide and show

--- app/src/main/java/com/example/scrutinizing_the_service/v2/CentralNavigation.kt
+++ app/src/main/java/com/example/scrutinizing_the_service/v2/CentralNavigation.kt
@@ -1,16 +1,23 @@
 package com.example.scrutinizing_the_service.v2
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.scrutinizing_the_service.v2.list.MusicListUI
 
 @Composable
-fun NavigationCentral() {
+fun NavigationCentral(
+    modifier: Modifier = Modifier
+) {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = ScreenName.AUDIO_LIST) {
+    NavHost(
+        navController = navController,
+        startDestination = ScreenName.AUDIO_LIST,
+        modifier = modifier
+    ) {
         composable(ScreenName.AUDIO_LIST) {
             MusicListUI()
         }
---
+++ app/src/main/java/com/example/scrutinizing_the_service/v2/player/AudioPlayer.kt
@@ -0,0 +1,11 @@
+package com.example.scrutinizing_the_service.v2.player
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AudioPlayer(
+
+) {
+
+
+}
