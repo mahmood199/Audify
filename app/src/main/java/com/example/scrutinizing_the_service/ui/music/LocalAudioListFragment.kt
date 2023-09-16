ec1b782f6f91c2dbc182f05e5928aea29d574b47
Mahmood <gaudentas199@gmail.com>
2023-09-17 03:31:12 +0530

    Music Player UI/UX improvement (#28)
    * Added new fragment to show locally saved audio files
    
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>
    
    * Adde media pplayer Ui and its component
    
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>
    
    ---------
    
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>
    Co-authored-by: MSI KATANA <mohammed.mahmood@semaai.com>

---
+++ app/src/main/java/com/example/scrutinizing_the_service/ui/music/LocalAudioListFragment.kt
@@ -0,0 +1,14 @@
+package com.example.scrutinizing_the_service.ui.music
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.scrutinizing_the_service.R
+
+class LocalAudioListFragment : Fragment() {
+
+
+
+}
