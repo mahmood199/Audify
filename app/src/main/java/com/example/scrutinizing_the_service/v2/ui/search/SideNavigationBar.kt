ed315ff74bd87567cad27a5eadd8e2ed0be0defb
MSI KATANA <gaudentas199@gmail.com>
2023-09-20 15:46:39 +0530

    Side navigation bar added

--- app/src/main/java/com/example/scrutinizing_the_service/v2/ui/search/SideNavigationBar.kt
+++ app/src/main/java/com/example/scrutinizing_the_service/v2/ui/search/SideNavigationBar.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
