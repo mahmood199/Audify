b1861b422b82cbd3eec844ae3e9582d7a49385b2
Mahmood <gaudentas199@gmail.com>
2023-10-09 03:36:29 +0530

    Removed redundant classes

--- app/src/main/java/com/example/scrutinizing_the_service/v2/connection/Status.kt
+++
@@ -1,9 +0,0 @@
-package com.example.scrutinizing_the_service.v2.connection
-
-sealed class Status(val displayText: String) {
-    data object Available : Status("Available")
-    data object Unavailable : Status("Unavailable")
-    data object Losing : Status("Losing")
-    data object Lost : Status("Lost")
-    data object Inactive : Status("Inactive")
-}
