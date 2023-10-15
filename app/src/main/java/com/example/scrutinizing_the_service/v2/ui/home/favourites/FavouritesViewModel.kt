3bf82a80cada0924e3c0080473b469b9c23ef7a4
Mahmood <gaudentas199@gmail.com>
2023-10-15 06:50:43 +0530

    Favourites section added for remote and local songs

---
+++ app/src/main/java/com/example/scrutinizing_the_service/v2/ui/home/favourites/FavouritesViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.scrutinizing_the_service.v2.ui.home.favourites
+
+import androidx.lifecycle.ViewModel
+import com.example.scrutinizing_the_service.v2.data.repo.contracts.SongsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class FavouritesViewModel @Inject constructor(
+    private val songsRepository: SongsRepository
+) : ViewModel() {
+
+
+
+}
