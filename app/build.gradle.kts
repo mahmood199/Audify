869b07da8bd17762f3985888a3f91cabbb94359a
Mahmood <gaudentas199@gmail.com>
2023-10-18 01:04:57 +0530

    Migrated the groovy gradle to kotlin to version catalog (#64)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.api.dsl.Packaging
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -67,7 +69,7 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.3"
     }
-    packagingOptions {
+    fun Packaging.() {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
@@ -82,107 +84,77 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation(platform ("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.navigation:navigation-compose:2.7.4")
-    implementation("androidx.compose.material3:material3")
+    implementation(libs.androidx.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.android.material)
+    implementation(libs.constraintlayout)
+    implementation(libs.activity.compose)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.graphics)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.compose.material3)
+
     implementation("androidx.compose.material3:material3-window-size-class")
-    implementation("com.google.accompanist:accompanist-permissions:0.32.0")
-    implementation("com.google.accompanist:accompanist-navigation-animation:0.32.0")
-    implementation("com.google.accompanist:accompanist-pager:0.32.0")
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.27.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform ("androidx.compose:compose-bom:2023.08.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    val lifecycle_version = "2.5.1"
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version")
-    implementation("androidx.lifecycle:lifecycle-service:$lifecycle_version")
+    implementation(libs.navigation.compose)
+    implementation(libs.accompanist.permissions)
+    implementation(libs.accompanist.navigation.animation)
+    implementation(libs.accompanist.pager)
+    implementation(libs.accompanist.systemuicontroller)
 
-    val hiltVersion = "2.48"
-    implementation("com.google.dagger:hilt-android:$hiltVersion")
-    kapt("com.google.dagger:hilt-compiler:$hiltVersion")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.compose.ui.test.junit4)
+    debugImplementation(libs.compose.ui.tooling)
+    debugImplementation(libs.compose.ui.test.manifest)
 
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.lifecycle.service)
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+    implementation(libs.hilt.navigation.compose)
 
     // Media3 ExoPlayer
 
-    val media3_version = "1.1.1"
+    implementation(libs.media3.exoplayer)
+    implementation(libs.media3.exoplayer.dash)
+    implementation(libs.media3.exoplayer.hls)
+    implementation(libs.media3.exoplayer.rtsp)
+    implementation(libs.media3.ui)
+    implementation(libs.media3.ui.leanback)
+    implementation(libs.media3.session)
+    implementation(libs.media3.extractor)
+    implementation(libs.legacy.support.v4)
+    implementation(libs.glide)
 
-    // For media playback using ExoPlayer
-    implementation("androidx.media3:media3-exoplayer:$media3_version")
 
-    // For DASH playback support with ExoPlayer
-    implementation("androidx.media3:media3-exoplayer-dash:$media3_version")
-    // For HLS playback support with ExoPlayer
-    implementation("androidx.media3:media3-exoplayer-hls:$media3_version")
-    // For RTSP playback support with ExoPlayer
-    implementation("androidx.media3:media3-exoplayer-rtsp:$media3_version")
-
-    implementation("androidx.media3:media3-ui:$media3_version")
-    // For building media playback UIs for Android TV using the Jetpack Leanback library
-    implementation("androidx.media3:media3-ui-leanback:$media3_version")
-
-    // For exposing and controlling media sessions
-    implementation("androidx.media3:media3-session:$media3_version")
-
-    // For extracting data from media containers
-    implementation("androidx.media3:media3-extractor:$media3_version")
-
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
-
+    implementation(libs.kotlinx.collections.immutable)
 
     //KTOR
-    implementation("io.ktor:ktor-client-core:2.3.2")
-    implementation("io.ktor:ktor-client-android:2.3.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
-    implementation("io.ktor:ktor-client-logging:2.3.2")
-    implementation("io.ktor:ktor-gson:1.5.0")
-    implementation("io.ktor:ktor-client-serialization:2.3.2")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.2")
-    implementation("io.ktor:ktor-client-content-negotiation:2.3.2")
-    implementation("io.ktor:ktor-client-encoding:2.1.2")
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.android)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.ktor.client.logging)
+    implementation(libs.ktor.gson)
+    implementation(libs.ktor.client.serialization)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.client.encoding)
 
     //Brotli
-    implementation("org.brotli:dec:0.1.2")
+    implementation(libs.brotli.dec)
 
-
-    //Room
-    val room_version = "2.6.0-rc01"
-    implementation("androidx.room:room-ktx:$room_version")
-    implementation("androidx.room:room-runtime:$room_version")
-    //noinspection KaptUsageInsteadOfKsp
-    kapt("androidx.room:room-compiler:$room_version")
-    implementation("androidx.room:room-paging:$room_version")
-
-
-    val paging_version = "3.2.1"
-    implementation("androidx.paging:paging-runtime:$paging_version")
-    implementation("androidx.paging:paging-compose:3.3.0-alpha02")
-
-
-    val glide_version = "2.2.8"
-    implementation("com.github.skydoves:landscapist-glide:$glide_version")
-
-    val dataStore_version = "1.0.0"
-    implementation("androidx.datastore:datastore-preferences:$dataStore_version")
-
-    val compose_audio_wave_form = "1.1.1"
-    implementation("com.github.lincollincol:compose-audiowaveform:$compose_audio_wave_form")
-
+    implementation(libs.room.ktx)
+    implementation(libs.room.runtime)
+    kapt(libs.room.compiler)
+    implementation(libs.room.paging)
+    implementation(libs.paging.runtime)
+    implementation(libs.paging.compose)
+    implementation(libs.landscapist.glide)
+    implementation(libs.datastore.preferences)
+    implementation(libs.compose.audiowaveform)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,4 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-/*
-buildscript {
-    dependencies {
-        classpath "com.google.dagger:hilt-android-gradle-plugin:2.48"
-        classpath "org.jetbrains.kotlin:kotlin-serialization:1.9.10"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21"
-    }
-}
-plugins {
-    id "com.android.application" version "8.3.0-alpha07" apply false
-    id "com.android.library" version "8.3.0-alpha07" apply false
-    id "org.jetbrains.kotlin.android" version "1.9.10" apply false
-    id "com.google.dagger.hilt.android" version "2.41" apply false
-}
-*/
 plugins {
     id("com.android.application") version "8.3.0-alpha07" apply false
     id("com.android.library") version "8.3.0-alpha07" apply false
