7e7d60940a59ab14c1fab66531524d9570509281
Mahmood <gaudentas199@gmail.com>
2024-01-29 15:15:58 +0530

    Removed the demonstrations related to service

--- service/src/main/java/com/download/service/MediaPlayerStatus.kt
+++
@@ -1,7 +0,0 @@
-package com.download.service
-
-data class MediaPlayerStatus(
-    val currentPlayingTime: Long,
-    val totalDuration: Long,
-    val isPlaying: Boolean
-)
--- service/src/main/java/com/download/service/ui/music/AudioPlayerFragment.kt
+++
@@ -1,9 +0,0 @@
-package com.download.service.ui.music
-
-import androidx.fragment.app.Fragment
-
-class AudioPlayerFragment : Fragment() {
-
-
-
-}
