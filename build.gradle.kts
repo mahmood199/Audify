0a1a3dc3166a5aa300ac56bfa49abca62d6e2e75
Mahmood <gaudentas199@gmail.com>
2024-02-12 15:18:29 +0530

    dependency resolution

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.3.0-alpha07" apply false
-    id("com.android.library") version "8.3.0-alpha07" apply false
+    id("com.android.application") version "8.4.0-alpha08" apply false
+    id("com.android.library") version "8.4.0-alpha08" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    id("com.google.dagger.hilt.android") version "2.41" apply false
-    id("com.android.test") version "8.3.0-alpha07" apply false
-    id("androidx.baselineprofile") version "1.2.0-beta05" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
+    id("com.android.test") version "8.4.0-alpha08" apply false
+    id("androidx.baselineprofile") version "1.2.3" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
     id("com.google.firebase.crashlytics") version "2.9.9" apply false
     id("com.google.firebase.firebase-perf") version "1.4.2" apply false
